b54d38d367e7c8cc43c93171765ccfce9a0301f2
Sumit Bhade <imsumitbhade@gmail.com>
2025-02-11 15:02:54 +0530

    Error handling with custom errors

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -4,6 +4,10 @@ import { useLoaderData } from "react-router-dom";
 function EventsPage() {
   const data = useLoaderData();
   const events = data.events;
+  // Way 1
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
 
   return <EventsList events={events} />;
 }
@@ -14,7 +18,10 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    //..
+    // Way 1
+    // return { isError: true, message: "Could not fetch data" };
+
+    throw { message: "Could not fetch events" };
   } else {
     return response;
   }
